b1e7546d1888d01c1e8b17cf46a242bcd7fd61e8
Crucial Jun <nich.otieno@gmail.com>
2023-05-24 13:44:41 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        Hello World
+      </div>
+      <h3>
+        Great job running JS
+      </h3>
+    </main>
+  )
+}
