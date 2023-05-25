d28f0865b878ea7aa1b3d10274004c7115a6b610
Crucial Jun <nich.otieno@gmail.com>
2023-05-25 17:01:46 +0300

    add images

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,43 @@
+'use client'
+import React, {useState} from 'react'
 
-import { GradientBackgroundCo } from '@/components/quote_generator/QuoteGeneratorElement'
-import Image from 'next/image'
+//Components Imports
+import { BackgroundImage1,BackgroundImage2, FooterCon, FooterLink, GradientBackgroundCo } from '@/components/quote_generator/QuoteGeneratorElement'
+
+//Assets Import
+import cloud1  from "../assets/cloud-and-thunder.png";
+import cloud2  from "../assets/cloudy-weather.png";
 
 
 export default function Home() {
+  const [numberOfQuotes, setNumberofQuotes] = useState<Number | null >(0)
   return (
 
       <GradientBackgroundCo>
+        
+        <BackgroundImage1
+          src={cloud1}
+          height={300}
+          alt='cloudybackground1'/>
+          <BackgroundImage2
+          src={cloud2}
+          height={300}
+          alt='cloudybackground2'/>
+          <FooterCon>
+            <>
+            Quotes Generated : {numberOfQuotes}
+            <br>
+            </br>
+            Developed with love by 
+            <FooterLink 
+              href="google.com"  
+              rel = "noopener noreferrer" 
+              target='_blank'> @Crucialjun </FooterLink> 
+            </>
+            
+
+          </FooterCon>
+        
       </GradientBackgroundCo>
       
 
--- components/quote_generator/QuoteGeneratorElement.tsx
+++ components/quote_generator/QuoteGeneratorElement.tsx
@@ -1,5 +1,7 @@
 'use client'
 import styled,{keyframes} from "styled-components"
+import Image from 'next/image'
+import Link from "next/link";
 
 
 const gradientAnimation = keyframes`
@@ -21,9 +23,80 @@ export const GradientBackgroundCo = styled.div`
     animation: ${gradientAnimation} 4s ease infinite;
     height: 100vh;
     width: 100vw;
-   
-    
+
   
+`;
+
+export const BackgroundImage1 = styled(Image)`
+    position: relative;
+    z-index: 1;
+    margin-left: -10px;
+    margin-top: -10px;
+
+`;
+export const BackgroundImage2 = styled(Image)`
+    position: fixed;
+    z-index: 1;
+    right: -120px;
+    bottom: -10px;
+
+`;
+
+export const FooterCon= styled.div`
+    width : 100vw;
+    height: 50px;
+    text-align : center;
+    font-family: "Source Code Pro" , monospace;
+    font-size : 15px;
+    position: absolute;
+    bottom : 0;
+    color : white;
+    z-index: 999999;
+
+`;
+
+export const FooterLink = styled(Link)`
+
+    color : white;
+    
+
+`;
+
+export const QuoteGeneretorContainer = styled.div`
+    background: linear-gradient(to right,#000046,#1cb5e0);
+    background-size: 400% 400%;
+    animation: ${gradientAnimation} 4s ease infinite;
+    height: 100vh;
+    width: 100vw;
+
+  
+`;
+export const QuoteGeneretorInnerContainer = styled.div`
+    background: linear-gradient(to right,#000046,#1cb5e0);
+    background-size: 400% 400%;
+    animation: ${gradientAnimation} 4s ease infinite;
+    height: 100vh;
+    width: 100vw;
+
+  
+`;
+export const QuoteGeneretorTitle = styled.div`
+    background: linear-gradient(to right,#000046,#1cb5e0);
+    background-size: 400% 400%;
+    animation: ${gradientAnimation} 4s ease infinite;
+    height: 100vh;
+    width: 100vw;
+
+  
+`;
+
+export const QuoteGeneretorSubTitle = styled.div`
+    background: linear-gradient(to right,#000046,#1cb5e0);
+    background-size: 400% 400%;
+    animation: ${gradientAnimation} 4s ease infinite;
+    height: 100vh;
+    width: 100vw;
+
   
 `;
 
